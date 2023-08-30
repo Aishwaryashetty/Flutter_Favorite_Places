13d55b4f26d839989ef60b84f742606fa72940b2
AishwaryaShetty <aishwaryapshetty@gmail.com>
2023-08-30 21:10:40 +0530

    initial commit - completed app

---
+++ lib/screens/map.dart
@@ -0,0 +1,70 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.location = const PlaceLocation(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: 'Google Office',
+    ),
+    this.isSelecting = true,
+  });
+
+  final PlaceLocation location;
+  final bool isSelecting;
+  @override
+  State<MapScreen> createState() {
+    return _MapScreen();
+  }
+}
+
+class _MapScreen extends State<MapScreen> {
+  LatLng? _pickedLocation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(widget.isSelecting ? 'Pick your Location' : 'Your Location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context, _pickedLocation);
+              },
+              icon: const Icon(Icons.save),
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        onTap: !widget.isSelecting
+            ? null
+            : (position) => setState(() {
+                  _pickedLocation = position;
+                }),
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.location.latitude,
+            widget.location.longitude,
+          ),
+          zoom: 16,
+        ),
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('M1'),
+                  position: _pickedLocation ??
+                      LatLng(
+                        widget.location.latitude,
+                        widget.location.longitude,
+                      ),
+                ),
+              },
+      ),
+    );
+  }
+}
